fefe6f54712d2f67b824b69eb2992c3c9335a8ab
Fedor Penin <patison4@yandex.ru>
2023-01-27 03:05:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# RF_SMOKES
+
+This is a Dad Fedor & Swift Dungeon framework for DayZ SA.
+
+## Features
+
+* [RF_SMOKES](docs/index.md)
+
+## Contributors
+
+* [romabeorn](https://github.com/romabeorn)
+* [Patison5](https://github.com/Patison5)
---
+++ config.cpp
@@ -0,0 +1,50 @@
+#define _ARMA_
+
+class CfgPatches
+{
+	class RF_SMOKES
+	{
+		author = "Dad Fedor & romabeorn";
+		name = "RF_SMOKES";
+		url = "https://discord.gg/d9kP7ezX3n";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] = {"DZ_Data", "DZ_Sounds_Effects"};
+		requiredVersion = 0.1;
+	};
+
+    class DZ_Gear_Camping
+    {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = 0.1;
+        requiredAddons[] = {"DZ_Data"};
+    };
+};
+
+class CfgMods
+{
+	class RF_SMOKES
+	{
+		type="mod";
+		dir="RF_SMOKES";
+		class defs
+		{
+			class gameScriptModule
+			{
+				value = "";
+				files[] = { "RF_SMOKES/scripts/3_Game" };
+			};
+			class worldScriptModule
+			{
+				value = "";
+				files[] = { "RF_SMOKES/scripts/4_World" };
+			};
+			class missionScriptModule
+			{
+				value = "";
+				files[] = { "RF_SMOKES/scripts/5_Mission" };
+			};
+		};
+	};
+};
